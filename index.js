38591cb7a3d0156de45888dfc60b55e9cf9bbde8
Luke Edwards <luke.edwards05@gmail.com>
2016-10-22 23:29:19 +0200

    upgrade plugin logic

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const assign = require("object-assign")
-const minify = require("uglify-js").minify
+'use strict';
+
+const extn = require('path').extname;
+const minify = require('uglify-js').minify;
 
 module.exports = function () {
-  this.filter("uglify", (data, options) => {
-    return assign({ ext: ".js" }, minify(data.toString(), assign({
-      fromString: true
-    }, options)))
-  })
-}
+	this.plugin('uglify', {}, function * (file, opts) {
+		opts = Object.assign({}, opts, {fromString: 1});
+
+		const ext = extn(file.base);
+		const rgx = new RegExp(ext, 'i');
+		// replace extension with `.js`
+		file.base = file.base.replace(rgx, '.js');
+
+		const out = minify(file.data.toString(), opts);
+
+		// write output
+		file.data = new Buffer(out.code);
+	});
+};
