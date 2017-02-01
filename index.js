6ab6af092f2e643fb75463b7085f0f03c872f147
Luke Edwards <luke.edwards05@gmail.com>
2017-01-31 17:11:06 -0800

    use fly param to mount

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const extn = require('path').extname;
-const minify = require('uglify-js').minify;
+const {extname} = require('path');
+const {minify} = require('uglify-js');
 
-module.exports = function () {
-	this.plugin('uglify', {}, function * (file, opts) {
+module.exports = function (fly) {
+	fly.plugin('uglify', {}, function * (file, opts) {
 		opts = Object.assign({}, opts, {fromString: 1});
 
-		const ext = extn(file.base);
+		const ext = extname(file.base);
 		const rgx = new RegExp(ext, 'i');
 		// replace extension with `.js`
 		file.base = file.base.replace(rgx, '.js');
