3a3f343b3d97fb6282149db545b067d5bb00892a
Luke Edwards <luke.edwards05@gmail.com>
2017-01-31 17:12:13 -0800

    update tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const join = require('path').join;
-const test = require('tape').test;
+const {join} = require('path');
+const test = require('tape');
 const Fly = require('fly');
 
 const dir = join(__dirname, 'fixtures');
@@ -11,29 +11,30 @@ test('fly-uglify', t => {
 	t.plan(4);
 
 	const fly = new Fly({
-		plugins: [{
-			func: require('../')
-		}],
+		plugins: [
+			require('../'),
+			require('fly-clear')
+		],
 		tasks: {
-			a: function * () {
+			* foo(f) {
 				const src = `${dir}/**/*.js`;
-				t.ok('uglify' in fly, 'attach `uglify()` plugin to fly');
+				t.ok('uglify' in fly.plugins, 'attach `uglify()` plugin to fly');
 
-				yield this.source(src).uglify().target(tmp);
-				const str1 = yield this.$.read(`${tmp}/a.js`, 'utf8');
+				yield f.source(src).uglify().target(tmp);
+				const str1 = yield f.$.read(`${tmp}/a.js`, 'utf8');
 				t.equal(str1, `console.log("this is a");`, 'apply `uglify-js` to content');
-				const arr1 = yield this.$.expand(`${tmp}/**/*.js`);
+				const arr1 = yield f.$.expand(`${tmp}/**/*.js`);
 				t.equal(arr1.length, 2, 'keep all files');
 
 				/* eslint camelcase:0 */
-				yield this.source(src).uglify({compress: {drop_console: 1}}).target(tmp);
-				const str2 = yield this.$.read(`${tmp}/a.js`, 'utf8');
+				yield f.source(src).uglify({compress: {drop_console: 1}}).target(tmp);
+				const str2 = yield f.$.read(`${tmp}/a.js`, 'utf8');
 				t.equal(str2, '', 'accept custom config (`compress` options)');
 
-				yield this.clear(tmp);
+				yield f.clear(tmp);
 			}
 		}
 	});
 
-	fly.start('a');
+	fly.start('foo');
 });
